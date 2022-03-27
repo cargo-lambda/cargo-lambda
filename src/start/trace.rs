0b48d9e9eb21234152c5dee6a523f868e9793da9
David Calavera <david.calavera@gmail.com>
2022-03-27 12:52:48 -0700

    Add support for XRay
    - Propagate trace ids from the invoke endpoint to functions.
    - Integrate with OpenTelemetry-AWS with Tracing.
    - Add flag to show traces in the standard output.
    
    Signed-off-by: David Calavera <david.calavera@gmail.com>

--- src/start/trace.rs
+++ src/start/trace.rs
@@ -1,4 +1,6 @@
 use axum::http::Request;
+use opentelemetry::{global, sdk::export::trace::stdout, sdk::trace};
+use opentelemetry_aws::trace::XrayPropagator;
 use tower_http::request_id::{MakeRequestId, RequestId};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 use uuid::Uuid;
@@ -13,8 +15,23 @@ impl MakeRequestId for RequestUuidService {
     }
 }
 
-pub(crate) fn init_tracing() {
+pub(crate) fn init_tracing(print_traces: bool) {
+    global::set_text_map_propagator(XrayPropagator::default());
+
+    let builder = stdout::new_pipeline().with_trace_config(
+        trace::config()
+            .with_sampler(trace::Sampler::AlwaysOn)
+            .with_id_generator(trace::XrayIdGenerator::default()),
+    );
+    let tracer = if print_traces {
+        builder.install_simple()
+    } else {
+        builder.with_writer(std::io::sink()).install_simple()
+    };
+    let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+
     tracing_subscriber::registry()
+        .with(telemetry)
         .with(tracing_subscriber::EnvFilter::new(
             std::env::var("RUST_LOG")
                 .unwrap_or_else(|_| "cargo_lambda=info,tower_http=info".into()),
