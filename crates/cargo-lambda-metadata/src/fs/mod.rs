a68ca39f24fb1f083f9c96e54eabfdf04f32a686
David Calavera <david.calavera@gmail.com>
2022-08-20 16:50:04 -0700

    Use copy_and_delete in all OSs
    Both Windows and MacOS can also fail with system errors
    moving files between drives.
    
    Signed-off-by: David Calavera <david.calavera@gmail.com>

--- crates/cargo-lambda-metadata/src/fs/mod.rs
+++ crates/cargo-lambda-metadata/src/fs/mod.rs
@@ -1,7 +1,2 @@
-#[cfg(not(target_os = "linux"))]
-pub use std::fs::rename;
-
-#[cfg(target_os = "linux")]
-mod rename_linux;
-#[cfg(target_os = "linux")]
-pub use rename_linux::*;
+mod rename;
+pub use rename::*;
