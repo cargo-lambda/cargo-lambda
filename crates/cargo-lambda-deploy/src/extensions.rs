9f70697d86d845bb3c4c3d519258e17161aa93fd
David Calavera <david.calavera@gmail.com>
2024-03-19 19:01:13 -0700

    Add more information to dry deploys. (#635)
    Signed-off-by: David Calavera <david.calavera@gmail.com>

--- crates/cargo-lambda-deploy/src/extensions.rs
+++ crates/cargo-lambda-deploy/src/extensions.rs
@@ -1,12 +1,10 @@
 use std::path::PathBuf;
 
-use crate::extract_tags;
-
 use super::DeployResult;
 use aws_sdk_s3::{primitives::ByteStream, Client as S3Client};
 use cargo_lambda_build::BinaryArchive;
 use cargo_lambda_interactive::progress::Progress;
-use cargo_lambda_metadata::cargo::function_deploy_metadata;
+use cargo_lambda_metadata::cargo::{function_deploy_metadata, DeployConfig};
 use cargo_lambda_remote::{
     aws_sdk_config::SdkConfig,
     aws_sdk_lambda::{
@@ -43,30 +41,25 @@ pub(crate) async fn deploy(
 ) -> Result<DeployResult> {
     let lambda_client = LambdaClient::new(sdk_config);
 
-    let mut deploy_metadata = function_deploy_metadata(manifest_path, name)
-        .into_diagnostic()?
-        .unwrap_or_default();
+    let deploy_metadata = function_deploy_metadata(
+        manifest_path,
+        name,
+        tags,
+        s3_bucket,
+        DeployConfig::default(),
+    )
+    .into_diagnostic()?;
 
     if let Some(extra_files) = &deploy_metadata.include {
         binary_archive.add_files(extra_files)?;
     }
 
-    let deploy_bucket = deploy_metadata
-        .s3_bucket
-        .as_ref()
-        .or(s3_bucket.as_ref())
-        .cloned();
-
-    let input = match &deploy_bucket {
+    let input = match &deploy_metadata.s3_bucket {
         None => LayerVersionContentInput::builder()
             .zip_file(Blob::new(binary_archive.read()?))
             .build(),
         Some(bucket) => {
             progress.set_message("uploading binary to S3");
-
-            if let Some(tags) = tags {
-                deploy_metadata.append_tags(extract_tags(tags));
-            }
 
             let s3_client = S3Client::new(sdk_config);
             let mut operation = s3_client
@@ -75,8 +68,8 @@ pub(crate) async fn deploy(
                 .key(name)
                 .body(ByteStream::from(binary_archive.read()?));
 
-            if tags.is_some() {
-                operation = operation.set_tagging(deploy_metadata.s3_tags());
+            if let Some(tags) = deploy_metadata.s3_tags() {
+                operation = operation.tagging(tags);
             }
 
             operation
