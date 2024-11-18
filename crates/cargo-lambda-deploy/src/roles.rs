6c890d8b6156a6531587473b19d172d1656377d7
perpil <97474956+perpil@users.noreply.github.com>
2024-11-17 19:35:27 -0800

    Move identity policy to its own statement, add sts:TagSession permissions required if you are assuming a role from a session with transitive tags

--- crates/cargo-lambda-deploy/src/roles.rs
+++ crates/cargo-lambda-deploy/src/roles.rs
@@ -27,10 +27,16 @@ pub(crate) async fn create(config: &SdkConfig, progress: &Progress) -> Result<St
         "Statement": [
             {
                 "Effect": "Allow",
-                "Action": ["sts:AssumeRole","sts:SetSourceIdentity"],
+                "Action": ["sts:AssumeRole"],
+                "Principal": {
+                    "Service": "lambda.amazonaws.com"
+                }
+            },
+            {
+                "Effect": "Allow",
+                "Action": ["sts:AssumeRole", "sts:SetSourceIdentity", "sts:TagSession"],
                 "Principal": {
                     "AWS": identity.arn().expect("missing account arn"),
-                    "Service": "lambda.amazonaws.com"
                 }
             }
         ]
@@ -64,8 +70,12 @@ pub(crate) async fn create(config: &SdkConfig, progress: &Progress) -> Result<St
 
     try_assume_role(&sts_client, role_arn).await?;
 
-    policy["Statement"][0]["Action"] = serde_json::json!("sts:AssumeRole");
-    policy["Statement"][0]["Principal"] = serde_json::json!({"Service": "lambda.amazonaws.com"});
+    // remove the current identity from the trust policy
+    policy["Statement"]
+        .as_array_mut()
+        .expect("missing statement array")
+        .pop();
+
     tracing::trace!(policy = ?policy, "updating assume policy");
 
     client
